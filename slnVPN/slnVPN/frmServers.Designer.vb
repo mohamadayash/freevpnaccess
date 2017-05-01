f4f47e617c9d171b65e342a4755e8facaa27e214
mohamadayash <mohamadayash@hotmail.com>
2017-05-01 18:15:25 +0300

    First Checkin

---
+++ slnVPN/slnVPN/frmServers.Designer.vb
@@ -0,0 +1,115 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmServers
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.gdvServers = New System.Windows.Forms.DataGridView()
+        Me.clnLocation = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.clnServer = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.clnProtocol = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.clUserName = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.clnPassword = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.RowClick = New System.Windows.Forms.DataGridViewButtonColumn()
+        CType(Me.gdvServers, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'gdvServers
+        '
+        Me.gdvServers.BackgroundColor = System.Drawing.Color.White
+        Me.gdvServers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.gdvServers.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.clnLocation, Me.clnServer, Me.clnProtocol, Me.clUserName, Me.clnPassword, Me.RowClick})
+        Me.gdvServers.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.gdvServers.Location = New System.Drawing.Point(0, 0)
+        Me.gdvServers.Name = "gdvServers"
+        Me.gdvServers.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None
+        Me.gdvServers.RowHeadersVisible = False
+        Me.gdvServers.Size = New System.Drawing.Size(668, 203)
+        Me.gdvServers.TabIndex = 0
+        '
+        'clnLocation
+        '
+        Me.clnLocation.DataPropertyName = "Location"
+        Me.clnLocation.HeaderText = "Location"
+        Me.clnLocation.Name = "clnLocation"
+        Me.clnLocation.ReadOnly = True
+        Me.clnLocation.Width = 120
+        '
+        'clnServer
+        '
+        Me.clnServer.DataPropertyName = "Server"
+        Me.clnServer.HeaderText = "Server"
+        Me.clnServer.Name = "clnServer"
+        Me.clnServer.ReadOnly = True
+        Me.clnServer.Width = 120
+        '
+        'clnProtocol
+        '
+        Me.clnProtocol.DataPropertyName = "Protocol"
+        Me.clnProtocol.HeaderText = "Protocol"
+        Me.clnProtocol.Name = "clnProtocol"
+        Me.clnProtocol.ReadOnly = True
+        '
+        'clUserName
+        '
+        Me.clUserName.DataPropertyName = "Username"
+        Me.clUserName.HeaderText = "User Name"
+        Me.clUserName.Name = "clUserName"
+        Me.clUserName.ReadOnly = True
+        Me.clUserName.Width = 120
+        '
+        'clnPassword
+        '
+        Me.clnPassword.DataPropertyName = "Password"
+        Me.clnPassword.HeaderText = "Password"
+        Me.clnPassword.Name = "clnPassword"
+        Me.clnPassword.ReadOnly = True
+        '
+        'RowClick
+        '
+        Me.RowClick.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill
+        Me.RowClick.HeaderText = ""
+        Me.RowClick.Name = "RowClick"
+        Me.RowClick.Text = "Connect"
+        '
+        'frmServers
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(668, 203)
+        Me.Controls.Add(Me.gdvServers)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.Name = "frmServers"
+        Me.Text = "VPN Servers"
+        CType(Me.gdvServers, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents gdvServers As System.Windows.Forms.DataGridView
+    Friend WithEvents clnLocation As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents clnServer As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents clnProtocol As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents clUserName As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents clnPassword As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents RowClick As System.Windows.Forms.DataGridViewButtonColumn
+
+End Class
---
+++ slnVPN/slnVPN/frmServers.vb
@@ -0,0 +1,91 @@
+﻿Imports System.Net
+Imports DotRas
+
+
+Public Class frmServers
+
+    Private ServersCollection As List(Of ServerEntry)
+    Private conn As RasConnection
+
+    Private Sub frmServers_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.gdvServers.AutoGenerateColumns = False
+
+        Dim httpRequest As HttpWebRequest = HttpWebRequest.Create("http://freevpnaccess.com/")
+        Dim httpResponse As HttpWebResponse = httpRequest.GetResponse()
+        Dim htmlStream = httpResponse.GetResponseStream()
+        Dim streamReader As New IO.StreamReader(htmlStream)
+        Dim strhtml As String = streamReader.ReadToEnd()
+
+
+        Dim htmlDocument As New HtmlAgilityPack.HtmlDocument
+        htmlDocument.LoadHtml(strhtml)
+
+        Dim arrServers = htmlDocument.DocumentNode.Descendants("table").FirstOrDefault.InnerHtml.Replace("<table>", "").Replace("</table>", "").Replace("<tr>", "").
+            Replace("</tr>", "").Replace("<tbody>", "").Replace("</tbody>", "").Replace("<td>", ",").Replace("</td>", "").Split(",")
+
+        ServersCollection = New List(Of ServerEntry)
+
+        For i As Integer = 6 To arrServers.Length - 1 Step 5
+            Dim Server As New ServerEntry
+            Server.Location = arrServers(i).Trim()
+            Server.Server = arrServers(i + 1).Trim()
+            Server.Protocol = arrServers(i + 2).Trim()
+            Server.Username = arrServers(i + 3).Trim()
+            Server.Password = arrServers(i + 4).Trim()
+            ServersCollection.Add(Server)
+        Next
+
+        Me.gdvServers.DataSource = ServersCollection
+
+
+
+    End Sub
+
+    Public Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles gdvServers.CellContentClick
+
+        Dim se As ServerEntry = Me.ServersCollection.Item(e.RowIndex)
+        Dim EntryName As String = "ehVPN"
+        Dim PhoneBook As New RasPhoneBook
+        PhoneBook.Open(RasPhoneBook.GetPhoneBookPath(RasPhoneBookType.User))
+
+        If (Not PhoneBook.Entries.Contains(EntryName)) Then
+            Dim entry As RasEntry = RasEntry.CreateVpnEntry(EntryName, se.Server, RasVpnStrategy.PptpOnly, DotRas.RasDevice.Create(EntryName, DotRas.RasDeviceType.Vpn))
+            PhoneBook.Entries.Add(entry)
+        End If
+
+
+        Dim dialer As New RasDialer()
+        dialer.EntryName = EntryName
+        dialer.PhoneBookPath = PhoneBook.Path
+
+        Dim oldentry = (From en As RasEntry In PhoneBook.Entries Where en.Name = EntryName Select en).FirstOrDefault
+        oldentry.PhoneNumber = se.Server
+        oldentry.Update()
+
+        Try
+            Try
+                conn = RasConnection.GetActiveConnectionByName(dialer.EntryName, dialer.PhoneBookPath)
+                conn.HangUp()
+            Catch ex As Exception
+
+            End Try
+            Dim cred As New System.Net.NetworkCredential(se.Username, se.Password)
+            dialer.Credentials = cred
+            dialer.Dial()
+        Catch ex As Exception
+
+        Finally
+            If (dialer Is Nothing) Then dialer.Dispose()
+        End Try
+    End Sub
+End Class
+
+Public Class ServerEntry
+    Public Property Location As String
+    Public Property Server As String
+    Public Property Protocol As String
+    Public Property Username As String
+
+    Public Property Password As String
+
+End Class
